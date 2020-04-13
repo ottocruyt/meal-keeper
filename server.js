67bed348df402ed60cee76ebf002357e968aa549
ottocruyt <ottocruyt@gmail.com>
2020-04-13 13:56:42 +0200

    prepare for production build on heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -10,12 +11,19 @@ connectDB();
 
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.json({ msg: "welcome to ..." }));
-
 // routes
 app.use("/api/contacts", require("./routes/contacts"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
+
+// serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
