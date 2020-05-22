c8cd42cabc5dbbc0a405ee952fa5c0ccd04841ca
ottocruyt <ottocruyt@gmail.com>
2020-05-22 16:09:42 +0200

    changes to server

--- server.js
+++ server.js
@@ -20,13 +20,19 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 
 // serve static assets in production
+app.use(express.static(path.join(__dirname, "client/build")));
+
 if (process.env.NODE_ENV === "production") {
-  // set static folder
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) =>
-    res.sendfile(path.resolve(__dirname, "client", "build", "index.html"))
-  );
+  app.use(express.static(path.join(__dirname, "client/build")));
+  //
+  app.get("*", (req, res) => {
+    res.sendfile(path.join((__dirname = "client/build/index.html")));
+  });
 }
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
