d131e0b65552acdd7613307fe7eb3e8c81f90676
ottocruyt <ottocruyt@gmail.com>
2020-05-22 16:43:16 +0200

    try

--- server.js
+++ server.js
@@ -20,19 +20,21 @@ app.use("/api/users", require("./routes/users"));
 app.use("/api/auth", require("./routes/auth"));
 
 // serve static assets in production
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname, "/client/build")));
+//build mode
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/public/index.html"));
+});
 
+/*
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static(path.join(__dirname, "../client/build")));
   //
   app.get("*", (req, res) => {
     res.sendfile(path.join((__dirname = "client/build/index.html")));
   });
 }
-//build mode
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"));
-});
+*/
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
